15f3127f3acadcc806f4dc8bc60bf7fca41e3cc6
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-27 16:58:25 +0200

    Change scripts at Column.js

--- Column.js
+++ Column.js
@@ -1,9 +1,20 @@
-function Column(name) {
+function Column(id, name) {
 	var self = this;
-	
-	this.id = randomString();
-	this.name = name;
+
+	this.id = id;
+	this.name = name || 'No name given';
 	this.element = createColumn();
+
+	deleteColumn: function() {
+    var self = this;
+    $.ajax({
+      url: baseUrl + '/column/' + self.id,
+      method: 'DELETE',
+      success: function(response){
+        self.element.remove();
+      }
+    });
+ }
 
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
@@ -12,17 +23,29 @@ function Column(name) {
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
 		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
-		
+
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
 		columnDelete.click(function() {
 			self.deleteColumn();
 		});
-		
-		columnAddCard.click(function(event) {
+
+		$columnAddCard.click(function(event) {
+			var cardName = prompt("Enter the name of the card");
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+    		url: baseUrl + '/card',
+    		method: 'POST',
+    		data: {
+    			name: cardName,
+    			bootcamp_kanban_column_id: self.id
+    		},
+    		success: function(response) {
+        	var card = new Card(response.id, cardName);
+        	self.createCard(card);
+    		}
+			});
 		});
-			
+
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
 		column.append(columnTitle)
 			.append(columnDelete)
