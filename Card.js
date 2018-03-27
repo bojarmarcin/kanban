079ef3b8f368ae97c29fd78937ee7c37e78e8edf
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-27 17:02:32 +0200

    Change scripts at Card.js

--- Card.js
+++ Card.js
@@ -1,24 +1,34 @@
 // KLASA KANBAN CARD
-function Card(description) {
+function Card(id, name) {
 	var self = this;
-	
-	this.id = randomString();
-	this.description = description;
+
+	this.id = id;
+	this.name = name || 'No name given';
 	this.element = createCard();
 
 	function createCard() {
 		var card = $('<li class="card"></li>');
 		var cardDeleteBtn = $('<button class="btn-delete">x</button>');
 		var cardDescription = $('<p class="card-description"></p>');
-		
-		cardDeleteBtn.click(function(){
-			self.removeCard();
-		});
-		
-		card.append(cardDeleteBtn);
-		cardDescription.text(self.description);
-		card.append(cardDescription)
-		return card;
+
+	removeCard: function() {
+	   var self = this;
+	   $.ajax({
+	     url: baseUrl + '/card/' + self.id,
+	     method: 'DELETE',
+	     success: function(){
+	       self.$element.remove();
+	     }
+	   });
+	}
+	cardDeleteBtn.click(function(){
+		self.removeCard();
+	});
+
+	card.append(cardDeleteBtn);
+	cardDescription.text(self.name);
+	card.append(cardDescription)
+	return card;
 	}
 }
 Card.prototype = {
